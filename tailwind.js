af96bc3fbbff5d4c165405a109087eb753df92fe
kenechi <nsudekenechi2@gmail.com>
2024-07-13 08:38:36 +0100

    first commit

---
+++ tailwind.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#2c2a2b',
+                secondary: "#F47323"
+
+            },
+            fontFamily: {
+                Ubuntu: "Ubuntu"
+            }
+        }
+    }
+}
