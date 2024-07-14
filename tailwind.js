4358ca9f2bbdc602ed4505fe2b1daf23a8b9cf87
kenechi <nsudekenechi2@gmail.com>
2024-07-14 23:23:24 +0100

    merge success

--- tailwind.js
+++ tailwind.js
@@ -8,14 +8,7 @@ tailwind.config = {
             },
             fontFamily: {
                 Ubuntu: "Ubuntu"
-<<<<<<< HEAD
-            },
-            fontFamily: {
-                aeonik: ['aeonik', 'Helvetica Neue', 'Helvetica', 'Roboto', 'Arial', 'sans-serif'],
-              },
-=======
             }
->>>>>>> 28b6e17f1c0734af6cf26a4ed92d17a30c951549
         }
     }
 }
