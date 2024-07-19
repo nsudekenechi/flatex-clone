1b73afafd2b32b7eb0fa1c103fef353c42e6b1c5
kenechi <nsudekenechi2@gmail.com>
2024-07-19 06:58:06 +0100

    added live data

--- main.js
+++ main.js
@@ -16,9 +16,36 @@ const swiper = new Swiper('.swiper', {
 
 });
 const key = "CG-4MjDXpJJ2oCvPhUmUG5a5dCz";
-const options = { method: 'GET', headers: { accept: 'application/json', Authorization: `Bearer ${key}` } };
+const coinList = document.querySelector("#coinList");
+const options = {
 
-fetch('https://pro-api.coingecko.com/api/v3/ping', options)
+    method: 'GET',
+    headers: { accept: 'application/json', Authorization: key },
+    // mode: "no-cors"
+};
+
+
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd', options)
     .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+    .then(coins => {
+        setTimeout(() => {
+            coinList.innerHTML = ""
+            coins.forEach((coin, index) => {
+                index + 1 <= 10 && (
+                    coinList.innerHTML += `   <tr class="border h-14 hover:bg-[#fcfcfc] duration-300">
+                    <td class="border">
+                        <div class="flex items-center justify-center gap-3">
+                            <img src='${coin.image}'alt=""
+                                class="w-5 h-5 object-cover">
+                            <span class="w-[10%] text-nowrap"> ${coin.name} <span class='uppercase'> (${coin.symbol})</span></span>
+                        </div>
+
+                    </td>
+
+                    <td class="border">${new Intl.NumberFormat("en-us", { currency: "USD", style: "currency" }).format(coin.current_price)}</td>
+                    <td class="border">${new Intl.NumberFormat("en-us", { currency: "USD", style: "currency" }).format(coin.current_price * 0.9)}</td>
+                </tr>`
+                )
+            })
+        }, 500)
+    }).catch(err => console.error(err));
