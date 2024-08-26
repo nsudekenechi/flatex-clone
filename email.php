e1f16958af34e7c036dc0b5591a0fc2651c4df9a
kenechi <nsudekenechi2@gmail.com>
2024-08-27 00:11:50 +0100

    new change added

--- email.php
+++ email.php
@@ -2,7 +2,14 @@
 if (isset($_POST["submit_ticket"])) {
     extract($_POST);
     print_r($_POST);
-    $message = "<p>Firstname: $first_name</p>
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: sender@example.com" . "\r\n";
+    $message = "
+    <html>
+    <head></head>
+    <body>
+        <p>Firstname: $first_name</p>
         <p>Lastname: $last_name</p>
             <p>Email: $email</p>
               <p>Phone Number: $phone_number</p>
@@ -12,6 +19,8 @@ if (isset($_POST["submit_ticket"])) {
                   <p>Market Price: $mkt_price</p>
                   <p>Sell Price: $sell_price</p>
                      <p>Quantity: $quantity</p>
+    </body>
+    </html>
     ";
 
     if (mail("nsudekenechi2@gmail.com", "New Ticket Submited", $message, "From: support@phanex.org")) {
